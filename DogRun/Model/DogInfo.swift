c2e7057398cb76431ce0d50cbb643a3f6255e4f4
devtoolsgg <lkk9193@gmail.com>
2024-03-01 19:36:14 +0900

    unit test 전 객체 builder pattern 적용

--- DogRun/Model/DogInfo.swift
+++ DogRun/Model/DogInfo.swift
@@ -5,10 +5,51 @@
 //
 
 struct DogInfo: Codable {
+    
     var uid: String
     var name: String
     var breed: String
     var birth: String
-    var gender: String
+    var gender: Gender
     var size: String
+    
+    // 빌더 구조체
+    struct Builder {
+        
+        private var dogInfo: DogInfo = DogInfo(uid: "", name: "", breed: "", birth: "", gender: .etc, size: "")
+         
+        mutating func setUID(_ uid: String) -> Builder {
+            dogInfo.uid = uid
+            return self
+        }
+         
+        mutating func setName(_ name: String) -> Builder {
+            dogInfo.name = name
+            return self
+        }
+        
+        mutating func setBreed(_ breed: String) -> Builder {
+            dogInfo.breed = breed
+            return self
+        }
+        
+        mutating func setBirth(_ birth: String) -> Builder {
+            dogInfo.birth = birth
+            return self
+        }
+        
+        mutating func setGender(_ gender: Gender) -> Builder {
+            dogInfo.gender = gender
+            return self
+        }
+        
+        mutating func setSize(_ size: String) -> Builder {
+            dogInfo.size = size
+            return self
+        }
+         
+        func build() -> DogInfo {
+            return dogInfo
+        }
+    }
 }
--- DogRun/Model/UserInfo.swift
+++ DogRun/Model/UserInfo.swift
@@ -6,10 +6,46 @@
 //
 
 struct UserInfo: Codable {
+    
     var uid: String
     var email: String
     var name: String
     var birth: String
     var area: String
-    var gender: Gender
+    
+    // 빌더 구조체
+    struct Builder {
+        
+        private var userInfo: UserInfo = UserInfo(uid: "", email: "", name: "", birth: "", area: "")
+         
+        mutating func setUID(_ uid: String) -> Builder {
+            userInfo.uid = uid
+            return self
+        }
+        
+        mutating func setEmail(_ email: String) -> Builder {
+            userInfo.email = email
+            return self
+        }
+        
+        mutating func setName(_ name: String) -> Builder {
+            userInfo.name = name
+            return self
+        }
+        
+        mutating func setBirth(_ birth: String) -> Builder {
+            userInfo.birth = birth
+            return self
+        }
+        
+        mutating func setArea(_ area: String) -> Builder {
+            userInfo.area = area
+            return self
+        }
+        
+        func build() -> UserInfo {
+            return userInfo
+        }
+    }
 }
+
